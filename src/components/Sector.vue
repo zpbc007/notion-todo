cad714f87fd77047580c2aaac7d1477a5b91730f
zhaopeng06 <zhaopeng06@kuaishou.com>
2022-03-06 16:06:23 +0800

    feat: sector work

---
+++ src/components/Sector.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="sector-wrapper">
+        <svg viewBox="0 0 100 100" class="sector">
+            <circle class="bg" cx="50" cy="50" r="45" />
+            <circle class="meter" cx="50" cy="50" r="45" />
+        </svg>
+        <div class="title">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+const props = defineProps<{
+    deg: number;
+}>();
+
+const offset = computed(() => 360 - props.deg);
+</script>
+
+<style scoped lang="less">
+.sector-wrapper {
+    position: relative;
+
+    .sector {
+        .bg {
+            fill: none;
+            stroke-width: 5px;
+            stroke: rgb(245, 245, 245);
+        }
+
+        .meter {
+            fill: none;
+            stroke-width: 5px;
+            stroke-linecap: round;
+            transform: rotate(-90deg);
+            transform-origin: 50% 50%;
+            stroke: #52c41a;
+            stroke-dasharray: 360;
+            stroke-dashoffset: v-bind(offset);
+            transition: stroke-dashoffset 0.5s;
+        }
+    }
+
+    .title {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+}
+</style>
